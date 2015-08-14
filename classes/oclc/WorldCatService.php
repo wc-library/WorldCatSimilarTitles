f1ca12b8da8146f381cd6ff318419a548de45458
bgarcia <brandon.garcia@my.wheaton.edu>
2015-08-14 13:39:12 -0500

    added flag to skip over failed queries, producing 1 error instead of several errors per query

--- classes/oclc/WorldCatService.php
+++ classes/oclc/WorldCatService.php
@@ -18,52 +18,55 @@ class WorldCatService {
             'library' => array(),
             'query' => array()
         );
-        $i=0;
+
         foreach ( \oclc\services\Catalog::batchQuery($idtype,$idlist,$params) as $id=>$result ) {
             $result = json_decode($result,true);
-            $query = array(
-                '@attributes' => array('idtype'=>$idtype),
-                'id'          => $id,
-                'title'       => $result['title'],
-                'author'      => $result['author'],
-                'publisher'   => $result['publisher'],
-                'date'        => $result['date'],
-                'OCLCnumber'  => $result['OCLCnumber'],
-                'url'         => "",
-                'related'     => array(),
-            );
-
-            foreach ($result[$idtype] as $relatedID) {
-                if ($relatedID == $id) {
-                    continue;
-                }
-                $query['related'][] = $relatedID;
-            }
-
-            if (isset($result['library'][0])) {
-                $libInfo = $result['library'][0];
-                if (isset($libInfo['opacUrl'])) {
-                    $query['url'] = $libInfo['opacUrl'];
-                }
-
-                if (isset($libInfo['institutionName']) && !isset($resultset['library']['institutionName'])) {
-                    $resultset['library'] = array(
-                        'institutionName' => $libInfo['institutionName'],
-                        'oclcSymbol'      => $libInfo['oclcSymbol'],
-                        'city'            => $libInfo['city'],
-                        'state'           => $libInfo['state'],
-                        'country'         => $libInfo['country'],
-                        'postalCode'      => $libInfo['postalCode']
-                    );
-                }
+            if (isset($result["diagnostic"])) {
+                error_log("Catalog search failed for $idtype/$id");
             } else {
-                $query['library'] = 'Holding not found!';
-            }
+                $query = array(
+                    '@attributes' => array('idtype'=>$idtype),
+                    'id'          => $id,
+                    'title'       => $result['title'],
+                    'author'      => $result['author'],
+                    'publisher'   => $result['publisher'],
+                    'date'        => $result['date'],
+                    'OCLCnumber'  => $result['OCLCnumber'],
+                    'url'         => "",
+                    'related'     => array(),
+                );
 
-            if (count($query['related'])) {
-                $resultset['query'][] = $query;
+                foreach ($result[$idtype] as $relatedID) {
+                    if ($relatedID == $id) {
+                        continue;
+                    }
+                    $query['related'][] = $relatedID;
+                }
+
+                if (isset($result['library'][0])) {
+                    $libInfo = $result['library'][0];
+                    if (isset($libInfo['opacUrl'])) {
+                        $query['url'] = $libInfo['opacUrl'];
+                    }
+
+                    if (isset($libInfo['institutionName']) && !isset($resultset['library']['institutionName'])) {
+                        $resultset['library'] = array(
+                            'institutionName' => $libInfo['institutionName'],
+                            'oclcSymbol'      => $libInfo['oclcSymbol'],
+                            'city'            => $libInfo['city'],
+                            'state'           => $libInfo['state'],
+                            'country'         => $libInfo['country'],
+                            'postalCode'      => $libInfo['postalCode']
+                        );
+                    }
+                } else {
+                    $query['library'] = 'Holding not found!';
+                }
+
+                if (count($query['related'])) {
+                    $resultset['query'][] = $query;
+                }
             }
-            ++$i;
         }
         return  $resultset;
     }
